80d8e94525ad4493146730a88c54d0996cacdfe2
maxcervi <massimocrv@gmail.com>
2021-06-04 17:23:25 +0200

    aggiunto commento

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const person = "pippo";
+const person = "pippo"; /* variabile non usata */
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
